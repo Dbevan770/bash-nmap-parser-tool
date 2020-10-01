c0ff7d93e4618b0ba2a4a3e5bcc450b0dde0f543
Danny <Gingerbreadpk@gmail.com>
2020-10-01 08:35:45 -0400

    Commented Code
    Added comments to the code explaining what it does.

--- nmap_parse.sh
+++ nmap_parse.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
 
+# Get the filepath from the user
 read -p "Enter the path to the nmap file: " file
 
+# Grep all the IPs and services and place them into one big array
 masterarray=($(cat $file | grep -E "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b|\b+\/tcp\s*open" | awk '{print $NF}'))
+
+# Create empty array to store IPs
 ips=()
+
+# Initialize a null string for storing current IP
 ip=""
+
+# Create an array of unique services found in the nmap file.
 services=($(awk '$0~/tcp\s+\open\s+(.+)/ { print $NF }' $file | sort | uniq))
+
+# Count variable to increment
 count=0
 
+# For loop to go thorough each unique service
 for ((x=0;x<${#services[@]};x++));do
-	count=0
-	ips=()
-	for ((i=0;i<${#masterarray[@]};++i));do
-		if [[ "${masterarray[i]}" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+
+        # Set count and IPs back to noting at the start of each service.
+        count=0
+        ips=()
+
+        # Loop through master array
+        for ((i=0;i<${#masterarray[@]};++i));do
+
+                # If the item in the master array is an IP set $ip to its value
+                if [[ "${masterarray[i]}" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
                         ip="${masterarray[i]}"
+                 fi
+
+                # If the item in the master array matches the service in the parent loop
+                if [[ "${masterarray[i]}" == "${services[x]}" ]]; then
+
+                        # If the current value of ip is not in the ips array add it add increment the count.
+                        if [[ ! "${ips[@]}" =~ "$ip" ]]; then
+                                ips+=($ip)
+                                ((count=count+1))
+                        fi
                 fi
+        done
 
-		if [[ "${masterarray[i]}" == "${services[x]}" ]]; then
-			if [[ ! "${ips[@]}" =~ "$ip" ]]; then
-				ips+=($ip)
-				((count=count+1))
-			fi
-		fi
-	done
-
-	printf "Service: ${services[x]} Count: $count\n"
-	printf "================================\n"
-	for ((y=0;y<${#ips[@]};++y));do
+        # Print the results
+        printf "Service: ${services[x]} Count: $count\n"
+        printf "================================\n"
+        for ((y=0;y<${#ips[@]};++y));do
 		printf "${ips[y]}\n"
-	done
-	printf "\n"
+        done
+        printf "\n"
 done
